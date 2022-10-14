064fca02897220971c015ca9388f881914ad0a2b
Miguel Oliveira <55458349+Miguel-Oliveiraa@users.noreply.github.com>
2022-10-14 17:34:29 -0300

    build: deleted storybook :heavy_minus_sign:

--- storybook-static/assets/preview.e84bd30d.js
+++
@@ -1,2 +0,0 @@
-import{t as o}from"./index.5616dad0.js";import"./iframe.5aec7f8e.js";import"./es.map.constructor.6f62c29c.js";import"./es.number.to-fixed.bb9d9f0c.js";import"./index.d74e9da1.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
-//# sourceMappingURL=preview.e84bd30d.js.map
